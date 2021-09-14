156a96f567393b4e05a695273f938e4297bbe822
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-09-14 16:41:27 +0300

    создание PostManager

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,15 @@
+package Manager;
+import domain.VkPost;
+
+import java.time.LocalTime;
+
+public class PostManager {
+    private VkPost[] posts;
+
+    public VkPost[] search(int id, int ownerId, String text, LocalTime dateTime) {
+        return null;
+    }
+    public void delete(int id) {
+
+    }
+}
