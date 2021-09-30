2233b7d3ddc2bf44ceca953bd4e8f284e3524d7d
Ekaterina Smetanina <AnenkowaK@mail.ru>
2021-09-30 14:51:12 +0300

    добавлен класс Views

---
+++ src/main/java/domain/Views.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Views {
+    private int countViews;
+
+    public int getCountViews() {
+        return countViews;
+    }
+
+    public void setCountViews(int countViews) {
+        this.countViews = countViews;
+    }
+}
--- src/main/java/domain/VkLike.java
+++ src/main/java/domain/VkLike.java
@@ -5,6 +5,24 @@ import java.time.LocalTime;
 public class VkLike {
     private int likes;
     private int ownerId;
+    private int canLike;
+    private int publish;
+
+    public int getCanLike() {
+        return canLike;
+    }
+
+    public void setCanLike(int canLike) {
+        this.canLike = canLike;
+    }
+
+    public int getPublish() {
+        return publish;
+    }
+
+    public void setPublish(int publish) {
+        this.publish = publish;
+    }
 
     public int getLikes() {
         return likes;
